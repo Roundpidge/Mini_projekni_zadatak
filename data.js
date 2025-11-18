d36591b74309ccf512b3398ad1a6ad11c0d6acbb
Sanja <sanjazitnik555@gmail.com>
2025-11-18 18:07:33 +0100

    Prepravljen post method tako da radi za sve liste, te dodane put i delete metode

--- data.js
+++ data.js
@@ -12,29 +12,29 @@ const cvijece = [
 ];
 
 const oprema = [
-  { id: 1, naziv: "Vrtne rukavice", cijena: 1.00 },
-  { id: 2, naziv: "Zaštitna mreža protiv insekata 65x220 cm", cijena: 0.50 },
-  { id: 3, naziv: "Hrana za biljke A+B", cijena: 5.00 },
-  { id: 4, naziv: "Sirkova metla S", cijena: 0.50 },
-  { id: 5, naziv: "Plastična kanta 10L", cijena: 2.00 },
-  { id: 6, naziv: "Vrtno crijevo 15m", cijena: 6.50 },
-  { id: 7, naziv: "Ručna lopatica za sadnju", cijena: 1.20 },
-  { id: 8, naziv: "Set za zalijevanje (prskalica + crijevo)", cijena: 8.00 },
-  { id: 9, naziv: "Zemlja za cvijeće 10kg", cijena: 3.50 },
-  { id: 10, naziv: "Keramička posuda za cvijeće", cijena: 4.00 }
+  { id: 1, naziv: "Vrtne rukavice", cijena: 1.00, kolicina: 5 },
+  { id: 2, naziv: "Zaštitna mreža protiv insekata 65x220 cm", cijena: 0.50, kolicina: 8 },
+  { id: 3, naziv: "Hrana za biljke A+B", cijena: 5.00, kolicina: 3 },
+  { id: 4, naziv: "Sirkova metla S", cijena: 0.50, kolicina: 9 },
+  { id: 5, naziv: "Plastična kanta 10L", cijena: 2.00, kolicina: 2 },
+  { id: 6, naziv: "Vrtno crijevo 15m", cijena: 6.50, kolicina: 5 },
+  { id: 7, naziv: "Ručna lopatica za sadnju", cijena: 1.20, kolicina: 6 },
+  { id: 8, naziv: "Set za zalijevanje (prskalica + crijevo)", cijena: 8.00, kolicina: 7 },
+  { id: 9, naziv: "Zemlja za cvijeće 10kg", cijena: 3.50, kolicina: 2},
+  { id: 10, naziv: "Keramička posuda za cvijeće", cijena: 4.00, kolicina: 3 }
 ];
 
 const posude = [
-  { id: 1, naziv: "Keramička posuda s uzorkom", promjer: 15, cijena: 3.50, boja: "bijela" },
-  { id: 2, naziv: "Plastična posuda za cvijeće", promjer: 20, cijena: 2.00, boja: "zelena" },
-  { id: 3, naziv: "Glinena tegla klasična", promjer: 25, cijena: 4.50, boja: "terakota" },
-  { id: 4, naziv: "Metalna posuda s ručkama", promjer: 18, cijena: 5.00, boja: "siva" },
-  { id: 5, naziv: "Staklena mini posuda", promjer: 10, cijena: 1.80, boja: "prozirna" },
-  { id: 6, naziv: "Betonska dekorativna posuda", promjer: 22, cijena: 6.00, boja: "siva" },
-  { id: 7, naziv: "Drvena posuda s postoljem", promjer: 30, cijena: 8.00, boja: "svijetlosmeđa" },
-  { id: 8, naziv: "Keramička viseća posuda", promjer: 16, cijena: 4.20, boja: "plava" },
-  { id: 9, naziv: "Plastična balkonska posuda", promjer: 40, cijena: 3.00, boja: "tamnosmeđa" },
-  { id: 10, naziv: "Tegla s automatskim zalijevanjem", promjer: 25, cijena: 7.50, boja: "crna" }
+  { id: 1, naziv: "Keramička posuda s uzorkom", promjer: 15, cijena: 3.50, boja: "bijela", kolicina: 10 },
+  { id: 2, naziv: "Plastična posuda za cvijeće", promjer: 20, cijena: 2.00, boja: "zelena", kolicina: 5 },
+  { id: 3, naziv: "Glinena tegla klasična", promjer: 25, cijena: 4.50, boja: "terakota", kolicina: 8 },
+  { id: 4, naziv: "Metalna posuda s ručkama", promjer: 18, cijena: 5.00, boja: "siva", kolicina: 3 },
+  { id: 5, naziv: "Staklena mini posuda", promjer: 10, cijena: 1.80, boja: "prozirna", kolicina: 9 },
+  { id: 6, naziv: "Betonska dekorativna posuda", promjer: 22, cijena: 6.00, boja: "siva", kolicina: 10 },
+  { id: 7, naziv: "Drvena posuda s postoljem", promjer: 30, cijena: 8.00, boja: "svijetlosmeđa", kolicina: 12 },
+  { id: 8, naziv: "Keramička viseća posuda", promjer: 16, cijena: 4.20, boja: "plava", kolicina: 4 },
+  { id: 9, naziv: "Plastična balkonska posuda", promjer: 40, cijena: 3.00, boja: "tamnosmeđa", kolicina: 5 },
+  { id: 10, naziv: "Tegla s automatskim zalijevanjem", promjer: 25, cijena: 7.50, boja: "crna", kolicina: 9 }
 ];
 
 module.exports = { cvijece, oprema, posude }
--- index.js
+++ index.js
@@ -5,81 +5,155 @@ app.use(express.json());
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-res.json('Hello, world!');
-res.json({cvijece, oprema, posude});
+    res.json('Hello, world!');
+    res.json({ cvijece, oprema, posude });
 });
 
 //CVIJECE METODE
 app.get('/cvijece', (req, res) => {
-res.json(cvijece);
+    res.json(cvijece);
 });
 
 app.get('/cvijece/:id', (req, res) => {
 
-    for(cvijet of cvijece){
-    const id_cvijet = req.params.id;
+    for (cvijet of cvijece) {
+        const id_cvijet = req.params.id;
 
         if (isNaN(id_pizza)) {
-        res.json({ message: 'Proslijedili ste parametar id koji nije broj!' });
+            res.json({ message: 'Proslijedili ste parametar id koji nije broj!' });
         }
 
-    const cvijet = cvijece.find(cvijet => cvijet.id == id_cvijet);
-        if(cvijet){
-        res.json(cvijece);
-        }else{
-            res.json({message: 'Cvijet s traženim id-om ne postoji'})
+        const cvijet = cvijece.find(cvijet => cvijet.id == id_cvijet);
+        if (cvijet) {
+            res.json(cvijece);
+        } else {
+            res.json({ message: 'Cvijet s traženim id-om ne postoji' })
         }
     }
 });
 
 //OPREMA METODE
-app.get('/oprema_i_posude', (req, res)=> {
-    res.json({oprema, posude});
+app.get('/oprema_i_posude', (req, res) => {
+    res.json({ oprema, posude });
 })
 
 app.get('/oprema/:id', (req, res) => {
-    for(alat of oprema){
-        const id_alat=req.params.id;
-        if(isNaN(id_alat)){
-            res.json({message: 'Proslijedili ste parametar id koji nije broj!'})
+    for (alat of oprema) {
+        const id_alat = req.params.id;
+        if (isNaN(id_alat)) {
+            res.json({ message: 'Proslijedili ste parametar id koji nije broj!' })
         }
-        const alat=oprema.find(alat=>alat.id==id_alat);
-        if(alat){
+        const alat = oprema.find(alat => alat.id == id_alat);
+        if (alat) {
             res.json(oprema)
-        }else{
-            res.json({message: 'Oprema s traženim id-om ne postoji!'})
+        } else {
+            res.json({ message: 'Oprema s traženim id-om ne postoji!' })
         }
     }
 
 });
 
 app.get('/posude/:id', (req, res) => {
-    for(posuda of posude){
-        const id_posuda=req.params.id;
-        if(isNaN(id_posuda)){
-            res.json({message: 'Proslijedili ste parametar id koji nije broj!'})
+    for (posuda of posude) {
+        const id_posuda = req.params.id;
+        if (isNaN(id_posuda)) {
+            res.json({ message: 'Proslijedili ste parametar id koji nije broj!' })
         }
-        const posuda=posude.find(posuda=>posuda.id==id_posuda);
-        if(posuda){
+        const posuda = posude.find(posuda => posuda.id == id_posuda);
+        if (posuda) {
             res.json(posude)
-        }else{
-            res.json({message: 'Posuda s traženim id-om ne postoji!'})
+        } else {
+            res.json({ message: 'Posuda s traženim id-om ne postoji!' })
         }
     }
 
 });
 
 //POST
-app.post('/naruci', (req, res) => {
-const narudzba = req.body;
-console.log('Primljeni podaci:', narudzba);
-res.send(`Vaša narudžba za ${cvijece.naziv} po cijeni ${cvijece.cijena} uspješno zaprimljena!`);
+app.post('/naruci/:lista', (req, res) => {
+    const { lista } = req.params;
+    const { naziv, kolicina } = req.body;
+    const baze = { cvijece, oprema, posude }
+
+    if (!baze[lista]) {
+
+        return res.status(400).send("Ta lista ne postoji!")
+    }
+
+    if (!naziv || !kolicina) {
+        return res.status(400).send("Nedostaje naziv ili kolicina! ")
+    }
+
+    const kolekcija = baze[lista]
+
+    const artikl = kolekcija.find(a => a.naziv.toLowerCase() === naziv.toLowerCase())
+    if (!artikl) {
+        res.status(400).send("Traženi artikl ne postoji")
+    }
+
+    if (artikl.kolicina < 1) {
+        return res.status(400).send("Nema dovoljno artikla na zalihama!")
+    }
+
+    console.log('Primljeni podaci:', narudzba);
+    res.send(`Vaša narudžba za ${narudzba.naziv} po cijeni ${narudzba.cijena} uspješno zaprimljena!`);
 });
 
-app.listen(PORT, error => {
-if (error) {
-console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
-} else {
-console.log(`Server je pokrenut na http://localhost:${PORT}`);
-}
+//PUT
+app.put('/:lista/:id', (req, res) => {
+    const { lista, id } = req.params;
+    const baze = { cvijece, oprema, posude }
+
+    const kolekcija = baze[lista]
+    if (!baze[lista]) {
+        return res.status(400).send("Ta lista ne postoji!")
+    }
+
+    const trazen = kolekcija.findIndex(t => t.id === id)
+    if (trazen === -1) {
+    return res.json({ message: 'lista s traženim id-om ne postoji' })
+    } 
+
+    const stara=kolekcija[trazen]
+
+    const nova={
+        ...stara,
+        ...req.body,
+        id: stara.id
+    }
+})
+
+//DELETE
+app.delete('/:lista/:id', (req, res) => {
+    const { lista, id } = req.params;
+
+    const baze = { cvijece, oprema, posude };
+    const kolekcija = baze[lista];
+
+    if (!kolekcija) {
+        return res.status(400).json({ message: 'Ta lista ne postoji!' });
+    }
+
+    const idNum = Number(id);
+    const indeks = kolekcija.findIndex(item => item.id === idNum);
+
+    if (indeks === -1) {
+        return res.status(404).json({ message: 'Artikl s traženim ID-em ne postoji.' });
+    }
+
+    kolekcija.splice(indeks, 1);
+
+    res.json({ message: 'Uspješno obrisano.' });
 });
+
+
+
+app.listen(PORT, error => {
+    if (error) {
+        console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
+    } else {
+        console.log(`Server je pokrenut na http://localhost:${PORT}`);
+    }
+});
+
+
